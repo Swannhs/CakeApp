d23501a4be5400e369955cb6ca7429e7896aca73
Swannhs <princeapurbask@gmail.com>
2021-01-21 15:44:28 -0500

    Delete operation is done. Introduce with cake validation

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -4,6 +4,7 @@ namespace App\Model\Table;
 
 use Cake\ORM\Table;
 use Cake\Utility\Text;
+use Cake\Validation\Validator;
 
 class ArticlesTable extends Table{
     public function initialize(array $config)
@@ -18,4 +19,17 @@ class ArticlesTable extends Table{
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
     }
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->allowEmptyString('title', false)
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->allowEmptyString('body', false)
+            ->minLength('body', 10);
+
+        return $validator;
+    }
+
 }
